27a84d349791816705cdf1f24d30a8ea36e177a0
Kilderson Sena <dersonsena@gmail.com>
2019-07-09 12:26:28 -0300

    parte de front parcialemente implementada

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/images/logo-devtube.png';
+import logoPhp from './assets/images/logo-php.png';
+import demoEditor from './assets/images/demo-code.png';
+import html2canvas from 'html2canvas';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      record: {
+        title: '',
+        language: '',
+        description: '',
+        screenshot: '',
+        snippet: ''
+      }
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      record: {
+        ...this.state.record,
+        [e.target.name]: [e.target.value]
+      },
+    });
+  };
+
+  download() {
+    const divStage = document.querySelector('div.stage');
+
+    html2canvas(divStage).then(function(canvas) {
+      document.body.appendChild(canvas);
+    });
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <h1 className="text-center">Gerador de Dica do Dev</h1>
+
+        <div className="row">
+          <div className="col-md-4">
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name='title'
+                  value={this.state.record.title}
+                  onChange={this.handleChange}
+                  required
+                  className='form-control'
+                  placeholder='Título da dica...' />
+              </div>
+
+              <div className="form-group">
+                <select
+                  name='language'
+                  value={this.state.record.language}
+                  onChange={this.handleChange}
+                  required
+                  className='form-control'
+                >
+                  <option>:: Linguagem da Dica ::</option>
+                  <option value='1'>Javascript</option>
+                  <option value='2'>PHP</option>
+                </select>
+              </div>
+
+              <div className="form-group">
+                <textarea
+                  name='description'
+                  required
+                  value={this.state.record.description}
+                  onChange={this.handleChange}
+                  className='form-control'
+                  placeholder='Descrição da dica...' />
+              </div>
+
+              <div className="form-group">
+                <input
+                  type='file'
+                  value={this.state.record.screenshot}
+                  onChange={this.handleChange}
+                  name='screenshot'
+                  required
+                  className='form-control'
+                  placeholder='Screenshot da dica...' />
+              </div>
+
+              <div className="form-group">
+                <textarea
+                  name='snippet'
+                  required
+                  value={this.state.record.snippet}
+                  onChange={this.handleChange}
+                  className='form-control'
+                  placeholder='Snippet de código...' />
+              </div>
+
+              <button type='submit' onClick={this.download} className="btn btn-primary btn-block btn-lg">
+                <i className="fa fa-download"></i> Download
+              </button>
+            </form>
+          </div>
+
+          <div className="col-md-8">
+            <div className="stage">
+              <header>
+                <img src={logoPhp} width="85" alt='Logo PHP' />
+                <h2 className="dica-title">
+                  { this.state.record.title }
+                </h2>
+              </header>
+              <div className='body'>
+                <p>{ this.state.record.description }</p>
+                <div className='screenshot'>
+                  <img src={demoEditor} alt="Code" />
+                </div>
+              </div>
+              <footer>
+                <img src={logo} width="120" alt='Logo Devtube' />
+                <ul>
+                  <li>
+                    <i className="fa fa-facebook-square"></i>
+                    <span>fb.com/devtubebr</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-twitter-square"></i>
+                    <span>@DevTube2</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-instagram"></i>
+                    <span>devtubebr</span>
+                  </li>
+                </ul>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default App;
